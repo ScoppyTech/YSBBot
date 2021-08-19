6c4b0ac4e1140b76cbd2100cddb3b4299ba6c490
StuckDuck <71439748+StuckDuckdotexe@users.noreply.github.com>
2021-08-19 16:51:41 +0200

    Update upd.py

--- gbot/upd.py
+++ gbot/upd.py
@@ -1,4 +1,7 @@
 import json, telegram, main
 with open local as local:
   json.loads(local)
-return {'gbot': local['__version__'], 'lib': telegram.__version__}
+
+version = {'gbot': local['__version__'], 'lib': telegram.__version__}
+print(version)
+return version
